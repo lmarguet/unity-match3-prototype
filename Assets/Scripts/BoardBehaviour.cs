886acce7df436dd3384f3ebc1084a2e4532348ed
Lucas Marguet <lucas.marguet@wooga.net>
2018-03-25 17:48:55 +0200

    Adds matching algorithms

--- Assets/Scripts/BoardBehaviour.cs
+++ Assets/Scripts/BoardBehaviour.cs
@@ -1,7 +1,10 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using Assets.Scripts.Extensions;
 using UnityEngine;
+using System.Linq;
+using Random = UnityEngine.Random;
 
 public class BoardBehaviour : MonoBehaviour
 {
@@ -29,6 +32,7 @@ public class BoardBehaviour : MonoBehaviour
         SetupTiles();
 
         FillRandom();
+        HighlightMatches();
     }
 
     #region Setup
@@ -61,7 +65,7 @@ public class BoardBehaviour : MonoBehaviour
         var verticalSize = (float) Height / 2 + BorderSize;
         var horizontalSize = (float) Width / 2 + BorderSize / aspectRatio;
 
-        camera.orthographicSize = verticalSize > horizontalSize ? verticalSize : horizontalSize;
+        camera.orthographicSize = Mathf.Max(verticalSize, horizontalSize);
     }
 
     private void FillRandom()
@@ -104,7 +108,7 @@ public class BoardBehaviour : MonoBehaviour
 
     private bool IsInBounds(int x, int y)
     {
-        return x >= 0 && y < Width
+        return x >= 0 && x < Width
             && y >= 0 && y < Height;
     }
 
@@ -132,28 +136,141 @@ public class BoardBehaviour : MonoBehaviour
         if (clickedTile != null && targetTile != null){
             SwapTiles(clickedTile, targetTile);
         }
+
         clickedTile = null;
         targetTile = null;
     }
 
     private void SwapTiles(TileBehaviour clicked, TileBehaviour target)
     {
-        if(!IsDirecNeighbor(clicked, target)) return;
-        
-        var gemA = allGameGems[clicked.xIndex, clicked.yIndex];
-        var gemB = allGameGems[target.xIndex, target.yIndex];
+        if (!IsDirecNeighbor(clicked, target)) return;
+
+        var gemA = GetGem(clicked.xIndex, clicked.yIndex);
+        var gemB = GetGem(target.xIndex, target.yIndex);
 
         gemA.Move(gemB.xIndex, gemB.yIndex, 0.1f);
         gemB.Move(gemA.xIndex, gemA.yIndex, 0.1f);
     }
-    
-    
+
 
     private bool IsDirecNeighbor(TileBehaviour tileA, TileBehaviour tileB)
     {
         return (Mathf.Abs(tileA.xIndex - tileB.xIndex) == 1 && tileA.yIndex == tileB.yIndex)
             || (Mathf.Abs(tileA.yIndex - tileB.yIndex) == 1 && tileA.xIndex == tileB.xIndex);
     }
+
+    #endregion
+
+
+    #region Matching
+
+    List<GameGemBehaviour> FindMatches(int xIndex, int yIndex, Vector2 searchDirection)
+    {
+        var matches = new List<GameGemBehaviour>();
+
+        var startGem = GetGem(xIndex, yIndex);
+        if (startGem == null){
+            throw new NullReferenceException("Start gem is null");
+        }
+
+        matches.Add(startGem);
+
+        var maxValue = Mathf.Max(Width, Height);
+
+        for (var i = 1; i < maxValue - 1; i++){
+            xIndex += (int) searchDirection.x;
+            yIndex += (int) searchDirection.y;
+
+            if (!IsInBounds(xIndex, yIndex)){
+                break;
+            }
+
+            var gem = GetGem(xIndex, yIndex);
+
+            if (gem.MatchType == startGem.MatchType){
+                matches.Add(gem);
+            }
+            else{
+                break;
+            }
+        }
+
+        return matches;
+    }
+
+
+    private List<GameGemBehaviour> FindVerticalMatches(int xIndex, int yIndex)
+    {
+        return FindAndCombineMatches(xIndex, yIndex, direction1:Vector2.down, direction2:Vector2.up);
+    }
+
+    private List<GameGemBehaviour> FindHorizontalMatches(int xIndex, int yIndex)
+    {
+        return FindAndCombineMatches(xIndex, yIndex, direction1:Vector2.left, direction2:Vector2.right);
+    }
+
+    private List<GameGemBehaviour> FindAndCombineMatches(int xIndex, int yIndex, Vector2 direction1, Vector2 direction2)
+    {
+        return
+            FindMatches(xIndex, yIndex, searchDirection: direction1)
+                .Union(
+                    FindMatches(xIndex, yIndex, searchDirection: direction2))
+                .ToList();
+    }
+
+    #endregion
+
+    #region Diagnostics
+
+    private void HighlightMatches()
+    {
+        for (var i = 0; i < Width; i++){
+            for (var j = 0; j < Height; j++){
+                var hMatches = FindHorizontalMatches(i, j);
+                var vMatches = FindVerticalMatches(i, j);
+
+                if (hMatches.Count < 3){
+                    hMatches = new List<GameGemBehaviour>();
+                }
+
+                if (vMatches.Count < 3){
+                    vMatches = new List<GameGemBehaviour>();
+                }
+
+                if (hMatches.Count == 0 && vMatches.Count == 0) continue;
+                ;
+
+                var combined = hMatches.Union(vMatches).ToList();
+
+                foreach (var gem in combined){
+                    var tileRender =
+                        GetTile(gem.xIndex, gem.yIndex)
+                            .GetComponent<SpriteRenderer>();
+
+                    tileRender.enabled = true;
+                    tileRender.color = gem.GetComponent<SpriteRenderer>().color;
+                }
+            }
+        }
+    }
+
+    #region Indexing
+
+    private TileBehaviour GetTile(int xIndex, int yIndex)
+    {
+        return IsInBounds(xIndex, yIndex)
+            ? tiles[xIndex, yIndex]
+            : null;
+    }
+
+    private GameGemBehaviour GetGem(int xIndex, int yIndex)
+    {
+        return IsInBounds(xIndex, yIndex)
+            ? allGameGems[xIndex, yIndex]
+            : null;
+    }
+
+    #endregion
 
     #endregion
 }
--- Assets/Scripts/GameGemBehaviour.cs
+++ Assets/Scripts/GameGemBehaviour.cs
@@ -2,68 +2,81 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameGemBehaviour : MonoBehaviour
 {
+    public enum MatchValue
+    {
+        Yellow,
+        Blue,
+        Magenta,
+        Indigo,
+        Green,
+        Teal,
+        Red,
+        Cyan,
+        Wild
+    }
+    
+    public int xIndex;
+    public int yIndex;
+    public MatchValue MatchType;
+    
+    private bool isMoving;
+    private BoardBehaviour board;
 
-	public int xIndex;
-	public int yIndex;
+    void Start() { }
 
-	private bool isMoving;
-	private BoardBehaviour board;
-	
-	void Start () {
-		
-	}
+    
 
 
-	public void SetCord(int x, int y)
-	{
-		xIndex = x;
-		yIndex = y;
-	}
+    public void SetCord(int x, int y)
+    {
+        xIndex = x;
+        yIndex = y;
+    }
 
-	public GameGemBehaviour Init(BoardBehaviour board)
-	{
-		this.board = board;
-		return this;
-	}
+    public GameGemBehaviour Init(BoardBehaviour board)
+    {
+        this.board = board;
+        return this;
+    }
 
-	public void Move(int destX, int destY, float timeToMove)
-	{
-		if(isMoving) return;
-		isMoving = true;
-		
-		StartCoroutine(
-			MoveRoutine(new Vector3(destX,destY), timeToMove)
-		);
-	}
+    public void Move(int destX, int destY, float timeToMove)
+    {
+        if (isMoving) return;
+        isMoving = true;
 
-	// TODO replace with tween
-	IEnumerator MoveRoutine(Vector3 destination, float timeToMove)
-	{
-		var startPosition = transform.position;
-		var reachedDestination = false;
-		var elapsedTime = 0f;
+        StartCoroutine(
+            MoveRoutine(new Vector3(destX, destY), timeToMove)
+        );
+    }
 
-		while (!reachedDestination){
-			elapsedTime +=  Time.deltaTime;
+    // TODO replace with tween
+    IEnumerator MoveRoutine(Vector3 destination, float timeToMove)
+    {
+        var startPosition = transform.position;
+        var reachedDestination = false;
+        var elapsedTime = 0f;
 
-			var lerpT = Mathf.Clamp(elapsedTime / timeToMove, 0, 1);
-			lerpT = Mathf.Sin(lerpT * Mathf.PI / 2f);
-			
-			transform.position = Vector3.Lerp(
-				a:startPosition, b:destination, t:lerpT
-			);
-			
-			reachedDestination = Vector3.Distance(transform.position, destination) < 0.01f;
-			
-			yield return null;
-		}
-		
-		board.PlaceGem(this, (int)destination.x, (int)destination.y);
-		
-		isMoving = false;
-	}
-	
+        while (!reachedDestination){
+            elapsedTime += Time.deltaTime;
+
+            var lerpT = Mathf.Clamp(elapsedTime / timeToMove, 0, 1);
+            lerpT = Mathf.Sin(lerpT * Mathf.PI / 2f);
+
+            transform.position = Vector3.Lerp(
+                a: startPosition, b: destination, t: lerpT
+            );
+
+            reachedDestination = Vector3.Distance(transform.position, destination) < 0.01f;
+
+            yield return null;
+        }
+
+        board.PlaceGem(this, (int) destination.x, (int) destination.y);
+
+        isMoving = false;
+    }
 }
